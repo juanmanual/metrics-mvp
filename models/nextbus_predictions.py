b2a73ee5a01bcac9806628f653bf63585fb4e9c4
vicky11z <vicky11zhang@gmail.com>
2019-06-12 20:25:45 -0700

    wip

--- models/nextbus_predictions.py
+++ models/nextbus_predictions.py
@@ -5,52 +5,90 @@ import os
 from xml.etree import ElementTree
 from datetime import datetime
 
-from models import predictions as p, nextbus
+from models import predictions as p, nextbus, util
 from . import util
 
-STOPS_STR = '&stops='
+
+def create_predictions_requests(agency_id: str) -> list:
+    # TODO: error if response includes error
+
+    # we can maximally split the string into num_routes/10 before the API complains that the URI is too long
+    multi_stops = util.get_routes_to_stops_str(agency_id, 10)
+    requests = []
+    for stops in multi_stops:
+        requests.append(
+            f"http://webservices.nextbus.com/service/publicJSONFeed?command=predictionsForMultiStops&a={agency_id}{stops}")
+    return requests
 
 
-def generate_all_stops(agency_id: str) -> str:
-    route_infos = nextbus.get_route_list(agency_id)
-    route_ids = map(lambda route_info: route_info.id, route_infos)
-    route_id_to_stop_ids = {}
-    for route_id in route_ids:
-        route_config = nextbus.get_route_config(agency_id, route_id)
-        route_id_to_stop_ids[route_id] = route_config.get_stop_ids()
-
-    all_stops = STOPS_STR
-    for route, stop_ids in route_id_to_stop_ids.items():
-        stops = [f"{route}|{stop_id}" for stop_id in stop_ids]
-        all_stops += STOPS_STR.join(stops)
-    return f"{STOPS_STR}{all_stops}"
-
-
-def get_prediction_data(agency_id: str, route_id: str, stop_id: str):
-    queried_time = datetime.Now()
+def get_prediction_data(agency_id: str):
+    queried_time = datetime.now()
     if re.match(r'^[\w\-]+$', agency_id) is None:
         raise Exception(f"Invalid agency id: {agency_id}")
 
-    cache_path = os.path.join(
-        util.get_data_dir(), f"prediction_{agency_id}_{route_id}_{stop_id}.json")
+    # TODO: cache responses
+    route_id_to_predictions = {}
+    for request_url in create_predictions_requests(agency_id):
+        response = requests.get(request_url)
+        if response.status_code != 200:
+            print(request_url)
+            print(response.content)
+            return
 
-    response = requests.get(
-        f"http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a={agency_id}&r={route_id}&s={stop_id}")
+        route_id_to_predictions.update(
+            parse_prediction_response(queried_time, response))
+    return route_id_to_predictions
 
-    # TODO: error if response includes error
-    root = ElementTree.fromstring(response.content)
-    root_predictions = root.find('predictions')
-    directions = root_predictions.findall('directions')
-    all_predictions = {}
-    for direction in directions:
-        predictions = direction.findall('prediction')
-        for p in predictions:
-            p_vals = p.attrib
-            dirTag = p_vals[dirTag]
-            if dirTag not in all_predictions:
-                all_predictions[dirTag] = []
-            curr_prediction = p.Prediction(
-                route_id, stop_id, p_vals, queried_time)
-            all_predictions[dirTag].append(curr_prediction)
-    return p.Predictions(
-        all_predictions, route_id, stop_id, queried_time)
+
+def parse_prediction_response(queried_time, resp):
+    route_id_to_predictions = {}
+    resp_json = resp.json()
+    predictions_by_route = resp_json['predictions']
+    for prediction_info in predictions_by_route:
+        # print(prediction_info)
+        route_id = prediction_info['routeTag']
+        stop_id = prediction_info['stopTag']
+        if 'direction' not in prediction_info:
+            continue
+        directions = prediction_info['direction']
+        if isinstance(directions, list):
+            # meaning there are multiple directions at this stop
+            for direction in directions:
+                predictions = direction['prediction']
+                curr_map = gen_route_id_to_predictions(
+                    route_id, stop_id, queried_time, predictions, route_id_to_predictions)
+                route_id_to_predictions.update(curr_map)
+        else:
+            predictions = directions['prediction']
+            curr_map = gen_route_id_to_predictions(
+                route_id, stop_id, queried_time, predictions, route_id_to_predictions)
+            route_id_to_predictions.update(curr_map)
+
+    return route_id_to_predictions
+
+
+def gen_route_id_to_predictions(route_id, stop_id: str, queried_time, predictions: list, route_id_to_predictions: dict):
+    if isinstance(predictions, list):
+        for prediction in predictions:
+            new_prediction = gen_prediction(
+                prediction, route_id, stop_id, queried_time)
+            if route_id not in route_id_to_predictions:
+                route_id_to_predictions[route_id] = []
+            route_id_to_predictions[route_id].append(new_prediction)
+    else:
+        new_prediction = gen_prediction(
+            predictions, route_id, stop_id, queried_time)
+        if route_id not in route_id_to_predictions:
+            route_id_to_predictions[route_id] = []
+            route_id_to_predictions[route_id].append(new_prediction)
+    return route_id_to_predictions
+
+
+def gen_prediction(prediction, route_id, stop_id, queried_time):
+    vehicle = prediction['vehicle']
+    minutes = prediction['minutes']
+    epoch_time = prediction['epochTime']
+    dir_tag = prediction['dirTag']
+    new_prediction = p.Prediction(
+        route_id, stop_id, vehicle, minutes, epoch_time, queried_time, dir_tag)
+    return new_prediction
--- models/util.py
+++ models/util.py
@@ -1,12 +1,18 @@
 from datetime import datetime, date, timedelta
 import os
 import pytz
+from models import nextbus
+
+STOPS_STR = '&stops='
+
 
 def parse_date(date_str):
-    (y,m,d) = date_str.split('-')
-    return date(int(y),int(m),int(d))
+    (y, m, d) = date_str.split('-')
+    return date(int(y), int(m), int(d))
 
 # todo: allow specifying day(s) of week
+
+
 def get_dates_in_range(start_date_str, end_date_str, max_dates=1000):
     start_date = parse_date(start_date_str)
     end_date = parse_date(end_date_str)
@@ -26,13 +32,16 @@ def get_dates_in_range(start_date_str, end_date_str, max_dates=1000):
             break
 
         if len(res) > max_dates:
-            raise Exception(f'too many dates between {start_date_str} and {end_date_str}')
+            raise Exception(
+                f'too many dates between {start_date_str} and {end_date_str}')
 
     return res
 
+
 def render_dwell_time(seconds):
     # remove 0 hours and replace 00 minutes with spaces to make it easier to scan column for large durations
-    return f'+{timedelta(seconds=round(seconds))}'.replace('+0:','+').replace('+00:','+  :')
+    return f'+{timedelta(seconds=round(seconds))}'.replace('+0:', '+').replace('+00:', '+  :')
+
 
 def get_data_dir():
     return f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}/data"
@@ -42,7 +51,7 @@ def get_timestamp_or_none(d: date, time_str: str, tz: pytz.timezone):
 
 def get_localized_datetime(d: date, time_str: str, tz: pytz.timezone):
 
-    time_str_parts = time_str.split('+') # + number of days
+    time_str_parts = time_str.split('+')  # + number of days
 
     if len(time_str_parts[0].split(':')) == 2:
         format = "%Y-%m-%d %H:%M"
@@ -56,3 +65,38 @@ def get_localized_datetime(d: date, time_str: str, tz: pytz.timezone):
         dt = dt + timedelta(days=int(time_str_parts[1]))
 
     return tz.localize(dt)
+
+
+'''
+Generate a list of strings in the format '&stops={route_id}|{stop_id}...'
+Since we can't substitute every stop on every route into an api call at once,
+segment the number of strings created by num_routes_per_str
+'''
+
+
+def get_routes_to_stops_str(agency_id: str, num_routes_per_str: int) -> str:
+    route_ids = [route.id for route in nextbus.get_route_list(agency_id)]
+    route_id_to_stop_ids = {}
+    for route_id in route_ids:
+        route_config = nextbus.get_route_config(agency_id, route_id)
+        route_id_to_stop_ids[route_id] = route_config.get_stop_ids()
+
+    route_strs = []
+    num_routes = len(route_ids)
+    for route_idx in range(0, num_routes, num_routes_per_str):
+        route_str = ''
+        for tmp_route_idx in range(route_idx, route_idx + num_routes_per_str-1):
+            if tmp_route_idx == num_routes:
+                break
+            route_id = route_ids[tmp_route_idx]
+            route_str += gen_stop_str_for_route(
+                route_id, route_id_to_stop_ids[route_id])
+        route_strs.append(route_str)
+    return route_strs
+
+
+def gen_stop_str_for_route(route_id: str, stop_ids: list) -> str:
+    all_stops = STOPS_STR
+    stops = [f"{route_id}|{stop_id}" for stop_id in stop_ids]
+    all_stops += STOPS_STR.join(stops)
+    return all_stops
